bf3f3c4e73cc1de4d724cd54e4d3ef0addfbcf1d
Jagannath Pandhare <Jagannath.k.Pandhare2@gmail.cm>
2026-07-30 00:05:28 +0530

    Phase 3, or useNavigate hooks completed

--- aircraft-maintenance-portal/src/pages/Login.jsx
+++ aircraft-maintenance-portal/src/pages/Login.jsx
@@ -1,5 +1,43 @@
+import { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+
 function Login() {
-    return <h1>Login Page</h1>
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+const navigate = useNavigate();
+
+    function handleLogin() {
+    
+        
+        if (!username) {
+            alert("Username is required");
+            return;
+        }
+        if (!password) {
+            alert("Password is required");
+            return;
+        }
+
+        if (username === "admin" && password === "admin123") {
+            navigate("/dashboard");
+        }
+        else {
+            alert("Invalid username or password.")
+        }
+    }
+
+    return (
+        <>
+            <h1>Login Page</h1>
+            <input type='text' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+            <br/>
+            <br/>
+            <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <br/>
+            <br/>
+            <button onClick={handleLogin}>Login</button>
+        </>
+    );
 }
 
 export default Login
