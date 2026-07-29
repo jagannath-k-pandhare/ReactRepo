599a1864a72410c8d46d658945cfae007c4ab156
Jagannath Pandhare <Jagannath.k.Pandhare2@gmail.cm>
2026-07-29 22:58:43 +0530

    Phase 2 completed

--- aircraft-maintenance-portal/src/App.jsx
+++ aircraft-maintenance-portal/src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import AircraftList from './pages/AircraftList'
+import AircraftDetails from './pages/AircraftDetails'
+import Favourite from './pages/Favourite'
+import Profile from './pages/Profile'
+import NotFound from './pages/NotFound'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Aircraft Maintenance Portal</h1>
+    <>
+      <h1>Aircraft Maintenance Portal</h1>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/aircraftlist" element={<AircraftList />} />
+        <Route path="/aircraftdetails/:id" element={<AircraftDetails />} />
+        <Route path="/favourite" element={<Favourite />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+    </>
   )
 }
 
--- aircraft-maintenance-portal/src/pages/AircraftDetails.jsx
+++ aircraft-maintenance-portal/src/pages/AircraftDetails.jsx
@@ -1,3 +1,7 @@
-function AircraftDetails() { 
-    return <h1>Aircraft Details</h1>
+import { useParams } from 'react-router-dom'
+
+function AircraftDetails(props) { 
+    const {id} = useParams()
+    return <h1>Aircraft Details and {id}</h1>
 }
+export default AircraftDetails
